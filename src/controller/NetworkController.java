b2d3bca0b5796dece234bc92f67816a1a770526d
Jay Honnold <jhonnold@mtu.edu>
2017-02-22 12:00:24 -0500

    removed server and adjusted so classes recognized by eclipse

--- src/controller/NetworkController.java
+++ src/controller/NetworkController.java
@@ -91,3 +91,4 @@ public class NetworkController implements Runnable, Closeable {
 	}
 	
 }
+
--- src/testing/Server.java
+++ src/testing/Server.java
@@ -2,28 +2,12 @@ package testing;
 
 import java.net.SocketException;
 
-import networking.UDPListener;
+import controller.NetworkController;
 import session.Session;
 
 public class Server {
 	
 	public static void main(String[] args) {
 		
-		UDPListener listener = null;
-		
-		try {
-			listener = new UDPListener("localserver");
-		} catch (SocketException e) {
-			System.out.println("Failed to start server!");
-			System.out.println(e.getMessage());
-		}
-		
-		Thread t = new Thread(listener, "Listener_Thread");
-		t.start();
-		
-		Session s = new Session();
-		listener.setCurrentSession(s);
-		
-		s.askQuestion("What is a+b?");
 	}	
 }
