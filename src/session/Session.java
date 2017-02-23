2fcc1abe85cc5001af56a9f56799d2417ad1df9c
zomby <zomby@jay-desktop>
2017-02-22 20:55:20 -0500

    Added commandline output and VERY basic session handling

--- src/session/Session.java
+++ src/session/Session.java
@@ -14,6 +14,8 @@ public class Session {
 		if (!clientList.contains(client)) {
 			System.out.println("New client, added as: " + client);
 			clientList.add(client);
+		} else {
+			System.out.println("Client already exists, not adding");
 		}
 	}
 }
