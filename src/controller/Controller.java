835cdd25c04c8b4d0adf3411a7912b627cf815ed
Jay Honnold <jhonnold@mtu.edu>
2017-02-27 17:46:35 -0500

    Added start() and stop()

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,23 +8,43 @@ import java.util.ArrayList;
 import networking.NetworkHandler;
 import session.Session;
 
-public class Controller implements Runnable {
+public class Controller {
+	
 	
 	private NetworkHandler network;
-	
 	private Session session = new Session();
 	
-	public Controller() throws SocketException {
+	private boolean running = false;
+	
+	public Controller() {}
+	
+	/**
+	 * Starts the network socket to start accepting
+	 * packets from clients
+	 * @throws SocketException -- if the port 9876 is in use
+	 */
+	public void start() throws SocketException {
 		try {
 			network = new NetworkHandler(this);
 		} catch (SocketException e) {
-			throw new SocketException("Could not launch listener!");
+			throw new SocketException("Failed to start the listening socket.");
 		}
+		
+		new Thread(network).start();
+		running = true;
 	}
 	
-	@Override
-	public void run() {
-		new Thread(network).start();
+	/**
+	 * Stops the network socket from accepting packets
+	 * from clients
+	 * @throws IllegalArgumentException -- if nothing is running
+	 */
+	public void stop() throws IllegalArgumentException {
+		if (!running) {
+			throw new IllegalArgumentException("Server is not currently active!");
+		}
+		
+		network.close();
 	}
 	
 	
