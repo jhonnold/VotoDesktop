d5de3e085a26dde23ed77232a12891e7db74d1ff
nlfosdic <nlfosdic@mtu.edu>
2017-03-01 11:52:34 -0500

    Voto-Desktop Changes

--- src/gui/VotoDesktop.java
+++ src/gui/VotoDesktop.java
@@ -1,39 +1,50 @@
 package gui;
 
-import java.awt.FlowLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.PrintStream;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import java.net.SocketException;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.Timer;
+import javax.swing.*;
 
 import controller.Controller;
 
 public class VotoDesktop implements Runnable, ActionListener {
 	
 	private JFrame f;
-	private Timer t = new Timer(100, this);
-	private JTextArea console = new JTextArea(24, 80);
-	private ConsoleOutput output = new ConsoleOutput(console, "Voto-Desktop");
+	private JButton hostButton, joinButton;
+	private JPanel startPanel;
+	private Timer t;
+	private JFileChooser fileChooser;
+	//private JTextArea console = new JTextArea(24, 80);
+	//private ConsoleOutput output = new ConsoleOutput(console, "Voto-Desktop");
 	
-	private Controller c  = null;
+	private Controller c;
 	
 	public VotoDesktop() {
 		
+		t = new Timer(100, this);
+		
 		f = new JFrame();
+		f.setSize(200, 200);
 		f.setLayout(new FlowLayout());
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		console.setEditable(false);
-		f.add(new JScrollPane(console));
-		System.setOut(new PrintStream(output));
+		startPanel = new JPanel(new GridLayout(3,1, 0, 25));
+		f.add(startPanel);
 		
-		f.pack();
+		startPanel.add(new JLabel("VOTO"));
+		
+		startPanel.add(hostButton = new JButton("Host Session"));
+		hostButton.addActionListener(this);
+		
+		startPanel.add(joinButton = new JButton("Join Session"));
+		joinButton.addActionListener(this);
+		
+		
+		//f.pack();
+		
+		f.setLocationRelativeTo(null);
 		f.setVisible(true);
 		t.start();
 	}
@@ -42,19 +53,29 @@ public class VotoDesktop implements Runnable, ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		f.repaint();
 		Toolkit.getDefaultToolkit().sync();
+		
+		if (e.getSource() == hostButton) {
+			f.remove(startPanel);
+			//System.setOut(new PrintStream(output));
+			try {
+				c.start();
+			} catch (SocketException se) {
+				se.printStackTrace();
+			}
+		}
+		if (e.getSource() == joinButton) {
+			fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));// + System.getProperty("file.separator")+ "Pictures"));
+			int result = fileChooser.showOpenDialog(startPanel);
+		}
 	}
 	
 	@Override
 	public void run() {
 		
-		try {
-			c = new Controller();
-		} catch (SocketException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		c = new Controller();
 			
-		c.run();		
+		//c.run();		
 	}
 	
 	public static void main(String[] args) {
