619599414b87b706a39e7cb0b7f0b8830b2ec9e5
nmwaara <nmwaara@mtu.edu>
2017-03-13 11:23:28 -0400

    Comments in Question class

--- src/session/Question.java
+++ src/session/Question.java
@@ -11,16 +11,34 @@ public class Question {
 	private int answer;
 	private HashMap<Integer, ArrayList<Client>> answerSet = new HashMap<>();
 	
+	/**
+	 * Constructor for building question
+	 * 
+	 * @param img - an image (byte array) associated with a given question
+	 * @param s - the current session the question is contained in
+	 */
 	public Question(ArrayList<byte[]> img, Session s) {
 		image = img;
 		currentSession = s;
 	}
 	
-	
+	/**
+	 * Sets the answer for the given question
+	 * 
+	 * @param ans - answer for the given question
+	 */
 	public void setAnswer(int ans) {
 		answer = ans;
 	}
 	
+	/**
+	 * Adds a client vote to the current question; keeps track of individual 
+	 * client votes for updating when required
+	 * 
+	 * @param clientID
+	 * @param clientVote
+	 * @return
+	 */
 	public int addVote(String clientID, String clientVote) {
 		
 		Client c = currentSession.getClient(clientID);
