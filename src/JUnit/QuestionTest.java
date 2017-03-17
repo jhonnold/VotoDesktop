71ee81d82e129332a3c71bb6857da63c3760d152
nlfosdic <nlfosdic@mtu.edu>
2017-03-17 15:20:14 -0400

    Added a JUnit for Question class

---
+++ src/JUnit/QuestionTest.java
@@ -0,0 +1,27 @@
+package JUnit;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import session.Question;
+import session.Session;
+
+public class QuestionTest {
+
+	Session s;
+	Question q;
+	
+	@Before
+	public void setup() throws Throwable {
+		s = new Session();
+		q = new Question(s, s.loadImage("testImage.png"), 123);
+	}
+	
+	@Test
+	public void test1() {
+		assertEquals(q.imageID(), 123);
+	}
+}
