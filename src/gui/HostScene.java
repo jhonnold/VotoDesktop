8a135bb0344d4411b638f2f510cab00daff5c066
nlfosdic <nlfosdic@mtu.edu>
2017-03-27 11:33:42 -0400

    Added handling for no file chosen

--- src/gui/HostScene.java
+++ src/gui/HostScene.java
@@ -85,8 +85,7 @@ public class HostScene extends Scene {
 
 		// Instantiate
 		fc = new FileChooser();
-		// fc.setInitialDirectory(new File(System.getProperty("user.home") +
-		// ".voto-desktop"));
+		// fc.setInitialDirectory(new File(System.getProperty("user.home") + ".voto-desktop"));
 		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("VOTO files (*.voto)", "*.voto");
 		fc.getExtensionFilters().add(extFilter);
 		file = fc.showOpenDialog(null);
@@ -95,7 +94,7 @@ public class HostScene extends Scene {
 		String filePath = null;
 		String answer = null;
 		
-		if (file.getPath().endsWith(".voto")) {
+		if (file != null && file.getPath().endsWith(".voto")) {
 			try {
 				txtScan = new Scanner(file);
 				while (txtScan.hasNext()) {
