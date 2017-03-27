005ecafccf3fb651eef97c7064c3f6461a211019
nmwaara <nmwaara@mtu.edu>
2017-03-27 12:30:59 -0400

    QuestionData and Display chart work

--- src/session/QuestionData.java
+++ src/session/QuestionData.java
@@ -5,27 +5,40 @@ import java.util.HashMap;
 
 public class QuestionData {
 
-	private Question currentQuestion;
 	private int ID;
-	private HashMap<Vote, ArrayList<Client>> questionAnswerData = new HashMap<>();
+	public HashMap<Vote, ArrayList<Client>> questionAnswerData = new HashMap<>();
 	
 	/**
 	 * QuestionData constructor
 	 */
 	public QuestionData(Question q, int qID) {
-		currentQuestion = q;
+			
+		if (q != null) {
+			questionAnswerData = q.getAnswerSet();
+		}
+		
 		ID = qID;
 	}
 	
-	/**
-	 * Gets the vote/client data for the current question
-	 * Will be used to create a list of question data for a session
-	 */
-	public HashMap<Vote, ArrayList<Client>> getQuestionVoteDate() {
-		questionAnswerData = currentQuestion.getAnswerSet();
-		return questionAnswerData;
+	@Override
+	public boolean equals(Object o) {
+		
+		if (!(o instanceof QuestionData)) {
+			return false;
+		}
+		
+		if (o == this) {
+			return true;
+		}
+		
+		QuestionData temp = (QuestionData) o;
+		
+		return temp.getID() == ID;
+		
 	}
-	
-	
+
+	public int getID() {
+		return ID;
+	}
 	
 }
--- src/session/Session.java
+++ src/session/Session.java
@@ -8,28 +8,31 @@ import java.io.IOException;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 import controller.Controller;
 
 public class Session {
-	
+
 	public final String ID;
 	private ArrayList<Client> clientList = new ArrayList<Client>();
-	
-	private Controller control = new Controller(this); 
+
+	private Controller control = new Controller(this);
 	private Question currentQuestion;
-	private QuestionData questionData;
-	
+	private ArrayList<Integer> questionList = new ArrayList<Integer>();
+	private ArrayList<QuestionData> dataList = new ArrayList<QuestionData>();
+
 	private int imageID = 1;
-	
+
 	public Session(String ID) {
 		this.ID = ID;
 	}
-	
+
 	/**
 	 * Starts a Voto session
+	 * 
 	 * @throws SocketException
 	 */
 	public void start() throws SocketException {
@@ -39,9 +42,10 @@ public class Session {
 			throw e;
 		}
 	}
-	
+
 	/**
 	 * Stops (or ends) the current Voto session
+	 * 
 	 * @throws IllegalArgumentException
 	 */
 	public void stop() throws IllegalArgumentException {
@@ -51,65 +55,74 @@ public class Session {
 			throw e;
 		}
 	}
-	
+
 	/**
 	 * the current Question
+	 * 
 	 * @return the current quesion
 	 */
 	public Question getCurrentQuestion() {
 		return currentQuestion;
 	}
-	
+
 	/**
-	 * Sets a new Question for the Session.
-	 * Loads image via filename and sets the correct answer to be
-	 * that of the inputted string.
-	 * @param filename The image file to be loaded
-	 * @param answer The answer string to be accepted 
+	 * Sets a new Question for the Session. Loads image via filename and sets
+	 * the correct answer to be that of the inputted string.
+	 * 
+	 * @param filename
+	 *            The image file to be loaded
+	 * @param answer
+	 *            The answer string to be accepted
 	 * @return True if loads properly, False if image fails
 	 */
 	public boolean setCurrentQuestion(String filename, String answer) {
-		
+
 		if (currentQuestion != null) {
+			dataList.add(new QuestionData(currentQuestion, imageID - 1));
 			currentQuestion.endQuestion();
 			for (Client c : clientList) {
 				c.setLastVote(null);
 			}
-			
+
 		}
-		
+
 		try {
 			ArrayList<byte[]> imageBytes = loadImage(filename);
-			currentQuestion = new Question(imageBytes, imageID++);
+			currentQuestion = new Question(imageBytes, imageID);
 			currentQuestion.setAnswer(answer);
+			questionList.add(imageID++);
 			return true;
 		} catch (IOException e) {
 			System.out.println("Failed to load image!");
 			return false;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Adds a new client to the session's client list with their ID
-	 * @param ID - the new client's ID
+	 * 
+	 * @param ID
+	 *            - the new client's ID
 	 */
 	public boolean addClient(String ID) {
 		if (clientList.contains(new Client(ID))) {
 			return false;
 		}
-		
+
 		System.out.println("Added new client: " + ID);
 		return clientList.add(new Client(ID));
 	}
-	
+
 	/**
 	 * Returns a client object based off of the client ID passed in
-	 * @param clientID - ID of the desired client
+	 * 
+	 * @param clientID
+	 *            - ID of the desired client
 	 * @return a client object
 	 */
 	public Client getClient(String clientID) {
-		
+
 		// Search the client list for the specified ID
 		for (Client c : clientList) {
 			if (c.getID().equals(clientID)) {
@@ -118,28 +131,31 @@ public class Session {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Adds a client vote to the current Question
-	 * @param clientID The string of the clients ID
-	 * @param vote The vote string
+	 * 
+	 * @param clientID
+	 *            The string of the clients ID
+	 * @param vote
+	 *            The vote string
 	 * @return false if not added or invalid, true if added
 	 */
 	public boolean addClientVote(String clientID, String vote, int voteNum) {
-		
+
 		Client c = getClient(clientID);
 		if (c == null) {
 			System.out.println("Client does not exist!");
 			return false;
 		}
-		
+
 		return currentQuestion.addVote(c, vote, voteNum);
-		
+
 	}
-	
-	
+
 	/**
 	 * Returns the ID for the image of the current question
+	 * 
 	 * @return ID of current question image
 	 */
 	public int getCurrentImageID() {
@@ -148,6 +164,7 @@ public class Session {
 
 	/**
 	 * Returns the number of packets for the current question image
+	 * 
 	 * @return image packet count
 	 */
 	public int getCurrentImagePacketCount() {
@@ -156,8 +173,11 @@ public class Session {
 
 	/**
 	 * Returns the image packet for the current question of the session
-	 * @param imageID - ID of the question image
-	 * @param packetNumber - corresponding packet number
+	 * 
+	 * @param imageID
+	 *            - ID of the question image
+	 * @param packetNumber
+	 *            - corresponding packet number
 	 * @return byte array packet for the current image
 	 * @throws IllegalArgumentException
 	 */
@@ -165,10 +185,10 @@ public class Session {
 		if (imageID != currentQuestion.imageID()) {
 			throw new IllegalArgumentException("Cannot request this image at this time " + imageID);
 		}
-		
+
 		return currentQuestion.getImagePacket(packetNumber);
 	}
-	
+
 	/**
 	 * Loads an image into an arraylist of bytearray of 64KB each
 	 * 
@@ -197,15 +217,14 @@ public class Session {
 
 		// Grab the byte array
 		bytearray = baos.toByteArray();
-		
-		
+
 		// cut the full array into 60 KB by using copyOfRange
 		for (int i = 0; i < bytearray.length; i += packetsize) {
 			packetBytes.add(Arrays.copyOfRange(bytearray, i, Math.min(bytearray.length, i + packetsize)));
 		}
-		
+
 		System.out.println("Loaded image completely");
-		
+
 		return packetBytes;
 
 		/**
@@ -218,30 +237,84 @@ public class Session {
 
 	/**
 	 * Returns the size (in bytes) of the current session image
+	 * 
 	 * @return image size in bytes
 	 */
 	public int getCurrentImageSize() {
 		int total = 0;
-		
+
 		for (byte[] b : currentQuestion.questionImg) {
 			total += b.length;
 		}
-		
+
 		return total;
 	}
-	
+
 	/**
 	 * If a current Question is loaded
+	 * 
 	 * @return - True if loaded, false if not (null)
 	 */
 	public boolean hasImage() {
 		return (currentQuestion != null);
 	}
+
+	/**
+	 * 
+	 * @param ID
+	 * @return
+	 */
+	public HashMap<Vote, Integer> returnQuestionDataBarChart(int ID) {
+		
+		HashMap<Vote, Integer> voteData = new HashMap<>();
+
+		QuestionData qd = null;
+
+		for (QuestionData temp : dataList) {
+
+			if (temp.getID() == ID) {
+				qd = temp;
+			}
+		}
+		if (qd != null) {
+			for (Vote v : qd.questionAnswerData.keySet()) {
+
+				voteData.put(v, qd.questionAnswerData.get(v).size());
+
+			}
+		}
+		
+		return voteData;
+	}
 	
 	/**
-	 * Gets the question data, separating from the question image
+	 * 
+	 * @param ID
+	 * @return
 	 */
-	public void parseQuestionData() {
-		//@TODO
+	public HashMap<Vote, Double> returnQuestionDataPieChart(int ID) {
+		
+		// TODO need to implement %, rather than number of votes
+		
+		HashMap<Vote, Double> voteData = new HashMap<>();
+
+		QuestionData qd = null;
+
+		for (QuestionData temp : dataList) {
+
+			if (temp.getID() == ID) {
+				qd = temp;
+			}
+		}
+		if (qd != null) {
+			for (Vote v : qd.questionAnswerData.keySet()) {
+
+				// need to implement with % rather than numerical vote count
+				//voteData.put(v, qd.questionAnswerData.get(v).size());
+
+			}
+		}
+		
+		return voteData;
 	}
 }
